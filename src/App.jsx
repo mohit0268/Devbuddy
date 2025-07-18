eea747e180dbaee3d9aabb8f6a94aa71bb301891
Mohit Purohit <mohitpurohit020@gmail.com>
2025-07-18 13:05:30 +0530

    added routes to login and profile page using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import Profile from "./components/Profile";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 
+
 function App() {
   return (
     <>
+    
       <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Body />}>
@@ -15,8 +17,10 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
+     
     </>
   );
 }
+
 
 export default App;
