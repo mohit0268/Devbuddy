00f940ca34716c0999379f9b69cf5079fd335734
Mohit Purohit <mohitpurohit020@gmail.com>
2025-07-18 22:46:09 +0530

    updates the styling and added new components

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import { useState } from "react";
+import axios from 'axios'
+import { useDispatch } from "react-redux";
+import { addUser } from "../../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import BASE_URL from '../../utils/CONSTANTS'
+
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email,setEmailID]= useState('mohit1@gmail.com');
+  const [password,setPassword] = useState('Mohit1@123');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  
 
-export default Login
+  const loginChangeHandler =async ()=>{
+    try {
+      const res = await axios.post(BASE_URL + '/login',{
+        email,
+        password
+      },{withCredentials:true});
+      dispatch(addUser(res.data));
+      return navigate('/');
+    } 
+    catch (error) {
+      console.error("Axios error occured",error);
+    }
+  }
+
+  return (
+    
+    <div className="flex justify-center">
+      <div className="card bg-neutral w-96 m-10">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div className="">
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Email address</legend>
+              <input
+                type="text"
+                className="input"
+                onChange={(e)=> setEmailID(e.target.value)}
+                value={email}
+              />
+            </fieldset>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Password</legend>
+              <input
+                type="password"
+                className="input"
+                onChange={(e)=> setPassword(e.target.value)}
+                value={password}
+              />
+            </fieldset>
+          </div>
+
+          <div className="card-actions justify-center mt-2">
+            <button className="btn btn-primary" onClick={loginChangeHandler}>Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,25 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  const user = useSelector((store) => store.user);
   return (
     <div className="navbar bg-base-300 shadow-sm px-8">
       <div className="flex-1">
         <a className="btn btn-ghost text-xl">DevBuddy</a>
       </div>
-      <div className="flex-none">
+      <div className="flex items-center">
+      
         <div className="dropdown dropdown-end">
           <div
             tabIndex={0}
             className="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow"
           ></div>
         </div>
-        <div className="dropdown dropdown-end">
+        {user && (
+          
+          <div className="dropdown dropdown-end flex items-center">
+            <div className="form-control px-2 font-semibold">Welcome, {user.firstName}</div>
           <div
             tabIndex={0}
             role="button"
@@ -22,7 +28,7 @@ const Navbar = () => {
             <div className="w-10 rounded-full">
               <img
                 alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                src="https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_640.png"
               />
             </div>
           </div>
@@ -41,6 +47,8 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
+        )}
+        
       </div>
     </div>
   );
