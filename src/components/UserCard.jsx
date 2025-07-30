12f9d95d708389198953a9eebd4c4ea9ac690014
Mohit Purohit <mohitpurohit020@gmail.com>
2025-07-30 11:41:02 +0530

    added usercard into /feed updated footer styling

---
+++ src/components/UserCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const UserCard = ({user}) => {
+  const {firstName,lastName,age,about,photo, gender} = user;
+  
+  return (
+    <div className="card bg-base-300 w-96 shadow-sm">
+      <figure>
+        <img
+          src={photo}
+          alt="user_avatar"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{`${firstName} ${lastName}`}</h2>
+        {age && gender && <p>{age + ", " + gender}</p>}
+        <p>{about}</p>
+        <div className="card-actions justify-center my-5">
+          <button className="btn btn-primary">Ignore</button>
+          <button className="btn btn-success">Interested</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
