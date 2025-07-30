e29199534b29974a2b77827493fd487f2c571262
Mohit Purohit <mohitpurohit020@gmail.com>
2025-07-30 11:51:18 +0530

    added a feed page

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import BASE_URL from "../../utils/CONSTANTS";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../../utils/feedSlice";
+import UserCard from "./userCard";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
+  
 
-export default Feed
+  const getFeed = async (res) => {
+    if(feed) return;
+    try {
+      const response = await axios.get(BASE_URL + "/feed", {
+        withCredentials: true,
+      });
+      dispatch(addFeed(response?.data));
+     } catch (error) {
+        res.status(400).send('Something went wrong!',error);
+    }
+  };
+  useEffect(() => {
+    getFeed();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return feed && (
+    <div className="flex justify-center my-10">
+    <UserCard user={feed[0]}/>
+    </div>
+  )
+};
+
+export default Feed;
